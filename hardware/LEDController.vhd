27d0165825bc789ca0b084eb809a293ba780947b
saada2006 <58188800+saada2006@users.noreply.github.com>
2025-04-03 13:06:23 -0400

    Individual duty cycle for each LED

--- hardware/LEDController.vhd
+++ hardware/LEDController.vhd
@@ -15,32 +15,33 @@ USE LPM.LPM_COMPONENTS.ALL;
 
 ENTITY LEDController IS
 PORT(
-    CLK			 : IN  STD_LOGIC;
-    EN_signals  : IN  STD_LOGIC_VECTOR(9 DOWNTO 0);
-    WRITE_EN    : IN  STD_LOGIC;
-    RESETN      : IN  STD_LOGIC;
-    LEDs        : OUT STD_LOGIC_VECTOR(9 DOWNTO 0);
-    IO_DATA     : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
+    CLK			: IN    STD_LOGIC;
+    EN_signals  : IN    STD_LOGIC_VECTOR(9 DOWNTO 0);
+    WRITE_EN    : IN    STD_LOGIC;
+    RESETN      : IN    STD_LOGIC;
+    LEDs        : OUT   STD_LOGIC_VECTOR(9 DOWNTO 0);
+    IO_DATA     : IN    STD_LOGIC_VECTOR(15 DOWNTO 0);
 
-    DBG_TIMER_OUT   : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
-	 DBG_DUTY_CYCLE_OUT : OUT STD_LOGIC_VECTOR(6 DOWNTO 0)
-    );
+	DBG_DUTY_CYCLE : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
+    DBG_TIMER : OUT STD_LOGIC_VECTOR(6 DOWNTO 0)
+);
 END LEDController;
 
 ARCHITECTURE a OF LEDController IS
-    SIGNAL TIMER        : STD_LOGIC_VECTOR(6 DOWNTO 0);
-    SIGNAL DUTY_CYCLE   : STD_LOGIC_VECTOR(6 DOWNTO 0);
-	 SIGNAL DUTY_CYCLE_NEXT	: STD_LOGIC_VECTOR(6 DOWNTO 0);
-BEGIN
-	-- This process block updates the brightnesses for LEDs
 
+    type DUTY_CYCLE_ARRAY is array (9 DOWNTO 0) of std_logic_vector(6 DOWNTO 0);
+
+    SIGNAL TIMER        : STD_LOGIC_VECTOR(6 DOWNTO 0);
+    SIGNAL DUTY_CYCLE   : DUTY_CYCLE_ARRAY;
+
+BEGIN
+	-- Update our timer 
     PROCESS(RESETN, CLK)
     BEGIN
 
         IF(RESETN = '0') THEN
             -- Set timer to zero
             TIMER <= "0000000";
-				DUTY_CYCLE <= "0000000";
 
         ELSIF(RISING_EDGE(CLK)) THEN
 
@@ -49,17 +50,32 @@ BEGIN
             IF(TIMER = "1100100") THEN
                 TIMER <= "0000000";
             END IF;
-				
-				IF (WRITE_EN = '1' AND or_reduce(EN_signals) = '1') THEN
-						DUTY_CYCLE <= IO_DATA(6 downto 0);
-				END IF;
-
+            
         END IF;
 
     END PROCESS;
-    -- Update all LEDs
-    LEDs <= "1111111111" when TIMER < DUTY_CYCLE else "0000000000";
 
-    DBG_TIMER_OUT <= TIMER;
-	DBG_DUTY_CYCLE_OUT <= DUTY_CYCLE;
+    -- Update duty cycle when EN_signal is updated.
+    -- Also update the LED signal
+    LEDUpdate: FOR i IN 9 DOWNTO 0 GENERATE
+        -- Update LEDs if EN_signal(i) is enabled
+        PROCESS(RESETN, EN_signals(i))
+        BEGIN
+
+            IF(RESETN = '0') THEN
+                DUTY_CYCLE(i) <= "0000000";
+            ELSIF(RISING_EDGE(EN_signals(i)) AND WRITE_EN = '1') THEN
+                DUTY_CYCLE(i) <= IO_DATA(6 downto 0);
+            END IF;
+
+        END PROCESS;
+
+        -- Update value of LED based on its relation to the timer
+        LEDs(i) <= '1' when TIMER < DUTY_CYCLE(i) else '0';
+
+    END GENERATE;
+
+    DBG_DUTY_CYCLE <= DUTY_CYCLE(1);
+    DBG_TIMER <= TIMER;
+
 END a;
